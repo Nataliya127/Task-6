1480e5cc556167aa8797aeb18648b375bfbf7795
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-04 11:33:32 +0300

    запустила программу проверки четности числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine ("Введите число");
+
+string number = Console.ReadLine();
+int number1 = int.Parse (number);
+
+
+            if (number1 % 2 == 1)
+            {
+                Console.WriteLine($"Число {number1} является нечетным");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number1} является четным");
+            }
+           
+
